275441ce474a1e41b0858d86e95c57ff6bb6744d
Owen Jacobson <owen@freshbooks.com>
2012-07-18 12:42:13 -0400

    Updated version number for release (1.3.1.freshbooks.4).
    I've switched the version number from using a dash to using a dot to simplify ops' life. Dashes are replaced with underscores in egg filenames, which makes version->URL transforms annoying.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 setup(
     name='cloudfiles',
-    version='1.3.1-freshbooks.3',
+    version='1.3.1.freshbooks.4',
     description='CloudFiles client library for python',
     author='Racklabs',
     url='https://www.mosso.com/',
